5af164aa73c32647597e895570ba759897f2eed3
Shravan Shandilya <Shravan Shandilya>
2017-02-15 10:07:40 +0530

    Getting PORT from OS's environmental varible

--- go-kalpas.go
+++ go-kalpas.go
@@ -5,11 +5,19 @@ import (
 	"net/http"
 )
 
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(determineListenAddress(), nil)
 }
