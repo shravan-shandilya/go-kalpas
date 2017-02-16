61e650ee8f48fe4bcafb404406a135e2c2def584
Shravan Shandilya <Shravan Shandilya>
2017-02-16 11:45:45 +0530

    Added unified api_handler and few test api endpoints

--- go-kalpas.go
+++ go-kalpas.go
@@ -1,34 +1,75 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func determineListenAddress() (string, error) {
-	port := os.Getenv("PORT")
-	if port == "" {
-		return "", fmt.Errorf("$PORT not set")
-	}
-	return ":" + port, nil
+func signin_handler(w http.ResponseWriter, request []string) error {
+	fmt.Fprintf(w, "Inside Signin handler")
+	return nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
+func signup_handler(w http.ResponseWriter, request []string) error {
+	fmt.Fprintf(w, "Inside Signup handler")
+	return nil
 }
-func main() {
-	addr, err := determineListenAddress()
-	if err != nil {
-		log.Fatal(err)
+
+func api_handler(w http.ResponseWriter, r *http.Request) {
+	request := strings.Split(r.URL.Path[8:], "/")
+	var err error
+	switch request[0] {
+	case "signin":
+		err = signin_handler(w, request[1:])
+		break
+	case "signup":
+		err = signup_handler(w, request[1:])
+		break
+	default:
+		fmt.Fprintf(w, "Invalid resource!", r.URL.Path[1:])
+		break
 	}
+	if err != nil {
+		http.Error(w, "Something went wrong", 500)
+	}
+}
+
+func database_init() {
+
+}
+
+func register_apis() {
+	//serve static content
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/api/v1/", api_handler)
+}
+
+func serve() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		//panic(fmt.Errorf("$PORT not set"))
+		port = strconv.FormatInt(5000, 10)
+	}
+
+	addr := ":" + port
+
 	log.Printf("Listening on %s...\n", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	database_init()
+
+	register_apis()
+
+	serve()
 }
